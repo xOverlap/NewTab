949e879525357f5f41b18b2a5c4f0c233b533855
xOverlap <f3v3s402@gmail.com>
2022-08-23 01:03:23 -0400

    Update clock.js

--- clock.js
+++ clock.js
@@ -21,7 +21,7 @@ function showTime() {
 	let hour = time.getHours();
 	let min = time.getMinutes();
 	let sec = time.getSeconds();
-	am_pm = "AM";
+	am_pm = " AM";
 
 	if (hour > 12) {
 		hour -= 12;
