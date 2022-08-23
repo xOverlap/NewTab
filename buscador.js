542407e6a8a7282f6f7871558c1ff19feb7b5a7c
xOverlap <f3v3s402@gmail.com>
2022-08-23 00:50:22 -0400

    Added more resposivity and YouCode search engine.

---
+++ buscador.js
@@ -0,0 +1,43 @@
+let input = document.querySelector(".input-buscador");
+
+let boton = document.querySelector(".buscar");
+
+function clear() {
+    input.value = "";
+}
+
+function clearInput() {
+    setTimeout(clear, 50);
+}
+
+function buscarGoogle() {
+    let url = 'https://www.google.com/search?q=' + input.value;
+    window.open(url, '_self');
+}
+
+function buscarYouCode() {
+    let url = 'https://you.com/search?q=' + input.value + "&tbm=youcode";
+    window.open(url, '_self');
+    clearInput();
+}
+
+
+
+boton.onclick = () => {
+    if (input.value === "") {
+    }
+    else {
+        buscarYouCode();
+    }
+}
+
+
+input.addEventListener( "keyup", e => {
+    if (e.key === "Enter") {
+        if (input.value === "") {
+        }
+        else {
+            buscarYouCode();
+        }
+    }
+})
