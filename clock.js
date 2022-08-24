98c3f891cb5dff914de50e5a29c082f3a0531729
xOverlap <f3v3s402@gmail.com>
2022-08-23 23:26:27 -0400

    New button and fix errors

--- clock.js
+++ clock.js
@@ -25,11 +25,11 @@ function showTime() {
 
 	if (hour > 12) {
 		hour -= 12;
-		am_pm = " PM";
+		am_pm = "PM";
 	}
 	if (hour == 0) {
 		hr = 12;
-		am_pm = " AM";
+		am_pm = "AM";
 	}
 
 	hour = hour < 10 ? "0" + hour : hour;
